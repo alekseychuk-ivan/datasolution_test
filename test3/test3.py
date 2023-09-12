2b406e5cc74087db6667158b7a2e7824201a3519
Alekseychuk_IA <Alekseychuk_IA@atomtex.com>
2023-09-12 16:56:42 +0300

    test 3 solved

--- test3/test3.py
+++ test3/test3.py
@@ -13,28 +13,34 @@ def polygon(image, coords, color):
     draw.polygon(coords, fill=color)
 
 
-
 def main():
     _, root = read_xml_full(maskpath)
-    dctmask = {'Skin': 'purple', 'Ignore': None}
+    dctmask = {'Skin': 'purple', 'Ignore': 'black'}
     dctblack = {'Skin': 'purple', 'Ignore': 'black'}
     for image in root.findall('image'):
         file = image.get('name').split('/')[-1]
-        width = int(image.get('width'))
-        height = int(image.get('height'))
-        immask = Image.open(os.path.join(path, file))
-        imblack = Image.new('RGB', (width, height), 'black')
-        # immask.show('original')
+        # width = int(image.get('width'))
+        # height = int(image.get('height'))
+        original = Image.open(os.path.join(path, file))
+        immask = original.copy()
+        mask = Image.new('L', original.size, 'white')
+        oblack = Image.new('RGB', original.size, 'black')
+        imblack = oblack.copy()
         for subtag in image:
             label = subtag.get('label')
             coords = list()
             for xy in subtag.get('points').split(';'):
                 xy = xy.split(',')
                 coords.append(tuple(map(int, (map(float, xy)))))
-            polygon(immask, coords, dctmask[label])
-            # polygon(imblack, coords, dctblack[label])
-        immask.show()
-        # imblack.show()
+            if label == 'Ignore':
+                polygon(mask, coords, dctmask[label])
+            else:
+                polygon(immask, coords, dctmask[label])
+                polygon(imblack, coords, dctblack[label])
+        original.paste(immask, mask)
+        original.save(Path(f'result/{file}'))
+        oblack.paste(imblack, mask)
+        oblack.save(Path(f'result/black_{file}'))
 
 
 if __name__ == '__main__':
